c0d845e795315d5925ce6a1ab98c6b323e3e2a8c
Мишенька <misenka@MacBook-Air-Misenka.local>
2026-08-19 16:16:28 +0300

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -17,32 +17,17 @@ export const referenceApi = axios.create({
   withCredentials: true,
 })
 
+function handleAuthErrors(error) {
+  if (error.response?.status == 401) {
+    window.location.href = 'https://sso.st29.ru/?url=timetrack.st29.ru'
+  } else if (error.response?.status == 403 && error.config?.notifyOn403) {
+    useNotificationStore().addNotification('Нет доступа к действию', 'error')
+  }
+  return Promise.reject(error)
+}
+
 permissionsApi.interceptors.response.use(
   (response) => response,
-  (error) => {
-    if (error.response?.status == 401) {
-      window.location.href = 'https://sso.st29.ru/?url=timetrack.st29.ru'
-      return
-    } else if (error.response?.status == 403) {
-      // window.location.href = '/'
-      return
-    }
-    return Promise.reject(error)
-  }
+  handleAuthErrors
 )
-
-timeTrackApi.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const notstore = useNotificationStore()
-    if (error.response?.status == 401) {
-      window.location.href = 'https://sso.st29.ru/?url=timetrack.st29.ru'
-      return
-    } else if (error.response?.status == 403) {
-      notstore.addNotification('Нет доступа к действию', 'error')
-      return error
-      // window.location.href = '/'
-    }
-    return Promise.reject(error)
-  }
-)
+timeTrackApi.interceptors.response.use((response) => response, handleAuthErrors)
